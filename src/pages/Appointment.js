3e71dbb73887f4b908c394ac8f53e09c16ba64cf
Adarsh Ninganur <adarsh.ninganur@gmail.com>
2022-01-15 22:58:06 +0530

    package css change

--- src/pages/Appointment.js
+++ src/pages/Appointment.js
@@ -2,13 +2,40 @@
 import React, { useState } from 'react'
 import DayTimePicker from '@mooncake-dev/react-day-time-picker';
 
+function timeSlotValidator(slotTime) {
+
+    // const morningTime = new Date(
+    //     slotTime.getFullYear(),
+    //     slotTime.getMonth(),
+    //     slotTime.getDate(),
+    //     09,
+    //     7,
+    //     0
+    // );
+
+    const eveningTime = new Date(
+        slotTime.getFullYear(),
+        slotTime.getMonth(),
+        slotTime.getDate(),
+        23,
+        7,
+        0
+    );
+    const isValid = slotTime.getTime() > eveningTime.getTime() ;
+
+    console.log("@@@ = ", isValid);
+    return isValid;
+}
 
 function Appointment() {
 
 
     return (
         <div className="Appointment">
-            <DayTimePicker timeSlotSizeMinutes={10} />;
+            <DayTimePicker
+                timeSlotSizeMinutes={15}
+                timeSlotValidator={timeSlotValidator}
+            />;
         </div>
     )
 }
