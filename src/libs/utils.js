0db85a11f65792806d5b4fe3acbb350cfec8b801
CHAITANYA-IN <chaitanyashinge@gmail.com>
2022-01-18 15:00:49 +0530

    Connect profile server with client

---
+++ src/libs/utils.js
@@ -0,0 +1,19 @@
+const addressTranslator = (a) => {
+  let address = '';
+  if (a?.houseNo) { address += `${a.houseNo}, ` }
+  if (a?.street) { address += `${a.street}, ` }
+  if (a?.landmark) { address += `${a.landmark}, ` }
+  if (a?.area) { address += `${a.area}, ` }
+  if (a?.district) { address += `${a.district}, ` }
+  if (a?.state) { address += `${a.state}, ` }
+  if (a?.country) { address += `${a.country} - ` }
+  if (a?.postalCode) { address += `${a.postalCode}` }
+  return address;
+}
+
+const nameTranslator = (n) => {
+  let name = `${n.firstName} ${n.middleName} ${n.lastName}`;
+  return name;
+}
+
+module.exports = { addressTranslator, nameTranslator };
