b4f4194cb478f00d11679d1c5ad410a1ee5c4df7
Ghanshyam Patil <ghanshyampatil1754@gmail.com>
2022-03-08 18:21:07 +0530

    temp

--- backend/routes/signin.js
+++ backend/routes/signin.js
@@ -9,44 +9,44 @@ const Password = require('../services/password');
 const router = express.Router();
 
 router.post("/", [
-    checkSchema(signInSchema),
-    validateRequest,
+checkSchema(signInSchema),
+validateRequest,
 ], async (req, res) => {
-    try {
-        console.log(req.body);
-        const isDoctor = req.body.isDoctor;
-        const Profile = isDoctor ? Doctor : Patient;
-        const emailid = req.body.email;
-        const pass = req.body.password;
-        const user = await Profile.findOne({
-            email: emailid
-        });
-        if (user && await Password.compare(user.password, pass)) {
-            
-            console.log("user verified",user._id);
-            const token = jwt.sign({id: user._id.toString(),email:user.email},process.env.SECRET,{expiresIn:"1h"});
-            console.log(token);
-            res.status(200).json({result:{
-                id: user.id,
-                username: user.name.firstName,
-                middleName: user.name.middleName,
-                lastName: user.name.lastName,
-                isDoctor:isDoctor,
-                token: token
-            },token :token});
-        } else {
-            // res.send("Password Wrong!!!!");
-            var data = { error: "Unauthorized Access!", message: "You have entered invalid credentials." }
-            console.log("Unauthorized Access!")
-            res.status(201).json(data);
-            // res.render("login", data);
-        }
-    } catch (error) {
-        // res.status(400).send(error);
-        var data = { error: "Unauthorized Access!", message: "server Error." }
-        console.log("in catch", error);
-        res.status(200).json(data);
+try {
+    console.log(req.body);
+    const isDoctor = req.body.isDoctor;
+    const Profile = isDoctor ? Doctor : Patient;
+    const emailid = req.body.email;
+    const pass = req.body.password;
+    const user = await Profile.findOne({
+        email: emailid
+    });
+    if (user && await Password.compare(user.password, pass)) {
+        
+        console.log("user verified",user._id);
+        const token = jwt.sign({id: user._id.toString(),email:user.email},process.env.SECRET,{expiresIn:"1h"});
+        console.log(token);
+        res.status(200).json({result:{
+            id: user.id,
+            username: user.name.firstName,
+            middleName: user.name.middleName,
+            lastName: user.name.lastName,
+            isDoctor:isDoctor,
+            token: token
+        },token :token});
+    } else {
+        // res.send("Password Wrong!!!!");
+        var data = { error: "Unauthorized Access!", message: "You have entered invalid credentials." }
+        console.log("Unauthorized Access!")
+        res.status(201).json(data);
+        // res.render("login", data);
     }
+} catch (error) {
+    // res.status(400).send(error);
+    var data = { error: "Unauthorized Access!", message: "server Error." }
+    console.log("in catch", error);
+    res.status(200).json(data);
+}
 });
 
 module.exports = router;
